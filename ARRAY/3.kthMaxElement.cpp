8ccb5dcb65c0102f6b8d83c0e13d23b8e9c8b24a
ASHISH ADHIKARI <56301864+Ashishadhikari123@users.noreply.github.com>
2022-01-28 12:56:42 +0530

    kth maximum element

--- ARRAY/3.kthMaxElement.cpp
+++ ARRAY/3.kthMaxElement.cpp
@@ -1,3 +1,4 @@
+//tc : O(nlogn)
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -15,6 +16,29 @@ int main()
     int k;
     cin>>k;
     sort(v.begin(),v.end(),greater<int>());
+    cout<<v[k-1];
+    return 0;
+}
+
+//tc : O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    int k;
+    cin>>k;
+    nth_element(v.begin(),v.begin()+k-1,v.end(),greater<int>());
     cout<<v[k-1];
     return 0;
 }
